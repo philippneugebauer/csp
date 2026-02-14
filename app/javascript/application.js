4021d39b779f1164ce9814353a8fa43397aff75e
Philipp Neugebauer <ppn@philippneugebauer.net>
2026-02-14 21:36:14 +0100

    bootstrap tooltips

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,26 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import * as bootstrap from "bootstrap"
+import "bootstrap"
+
+const setupTooltips = () => {
+  const Tooltip = window.bootstrap?.Tooltip
+  if (!Tooltip) return
+
+  document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach((el) => {
+    Tooltip.getOrCreateInstance(el)
+  })
+}
+
+const disposeTooltips = () => {
+  const Tooltip = window.bootstrap?.Tooltip
+  if (!Tooltip) return
+
+  document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach((el) => {
+    const instance = Tooltip.getInstance(el)
+    instance?.dispose()
+  })
+}
+
+document.addEventListener("turbo:load", setupTooltips)
+document.addEventListener("turbo:before-cache", disposeTooltips)
